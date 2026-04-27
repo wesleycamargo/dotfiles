79d8ec8736ca5ce7e1cabcef65842c89250a8739
Wesley Camargo <camargo.wes@gmail.com>
2026-04-27 22:43:43 +0200

    cli tools multi os

--- cli-tools.ps1
+++ cli-tools.ps1
@@ -1,3 +1,12 @@
+# Determine if sudo is needed (Linux non-root only)
+$isRoot = $IsLinux -and ((& id -u) -eq "0")
+$useSudo = $IsLinux -and -not $isRoot
+
+function Invoke-Sudo {
+    param([string]$Cmd, [string[]]$Args)
+    if ($useSudo) { & sudo $Cmd @Args } else { & $Cmd @Args }
+}
+
 # Dev Tools
 if ($IsWindows) {
     winget install --id Git.Git -e --source winget
@@ -5,11 +14,11 @@ if ($IsWindows) {
 }
 elseif ($IsLinux) {
     if (Get-Command apt-get -ErrorAction SilentlyContinue) {
-        sudo apt-get update
-        sudo apt-get install -y git
+        Invoke-Sudo apt-get @("update")
+        Invoke-Sudo apt-get @("install", "-y", "git")
     }
     elseif (Get-Command dnf -ErrorAction SilentlyContinue) {
-        sudo dnf install -y git
+        Invoke-Sudo dnf @("install", "-y", "git")
     }
     elseif (Get-Command brew -ErrorAction SilentlyContinue) {
         brew install git
@@ -23,21 +32,29 @@ if ($IsWindows) {
 }
 elseif ($IsLinux) {
     if (Get-Command apt-get -ErrorAction SilentlyContinue) {
-        curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+        $installScript = & curl -fsSL https://aka.ms/InstallAzureCLIDeb
+        if ($useSudo) { $installScript | sudo bash } else { $installScript | bash }
     }
     elseif (Get-Command dnf -ErrorAction SilentlyContinue) {
-        sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-        sudo dnf install -y azure-cli
+        Invoke-Sudo rpm @("--import", "https://packages.microsoft.com/keys/microsoft.asc")
+        Invoke-Sudo dnf @("install", "-y", "azure-cli")
     }
     elseif (Get-Command brew -ErrorAction SilentlyContinue) {
         brew install azure-cli
     }
 }
 
-az extension add --name azure-devops
-az extension add --name bicep
+# # Azure CLI extensions (only if az is available after install)
+# if (Get-Command az -ErrorAction SilentlyContinue) {
+#     az extension add --name azure-devops
+#     az extension add --name bicep
+# }
 
 
 # PowerShell Modules
+if (-not (Get-PSRepository -Name PSGallery -ErrorAction SilentlyContinue)) {
+    Register-PSRepository -Default
+}
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
 Install-Module -Name Az -Force -AllowClobber
-Install-Module -Name Az.DevOps -Force -AllowClobber
