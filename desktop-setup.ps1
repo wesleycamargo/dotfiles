ba8496a68bd6d9c92e8f7ee988167b19aa1a0805
Wesley Camargo <camargo.wes@gmail.com>
2026-04-27 23:52:43 +0200

    desktop tools

---
+++ desktop-setup.ps1
@@ -0,0 +1,6 @@
+#Dev Tools
+winget install --id Microsoft.VisualStudioCode -e --source winget
+
+#Work tools
+winget install --id Learnpulse.Screenpresso -e --source winget
+winget install --id Microsoft.Sysinternals.ZoomIt -e --source winget
--- desktop-tools.ps1
+++
@@ -1,6 +0,0 @@
-#Dev Tools
-winget install --id Microsoft.VisualStudioCode -e --source winget
-
-#Work tools
-winget install --id Learnpulse.Screenpresso -e --source winget
-winget install --id Microsoft.Sysinternals.ZoomIt -e --source winget
