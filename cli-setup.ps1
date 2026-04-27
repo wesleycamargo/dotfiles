aa066ccd1bb25305191fb87c35d1097e3a6c25cf
Wesley Camargo <camargo.wes@gmail.com>
2026-04-27 23:41:57 +0200

    terminal setup merge

--- cli-setup.ps1
+++
@@ -1,6 +0,0 @@
-$cliTools = "https://raw.githubusercontent.com/wesleycamargo/dotfiles/refs/heads/master/cli-tools.ps1"
-Invoke-Expression ((new-object net.webclient).DownloadString($cliTools))
-
-$url = "https://raw.githubusercontent.com/wesleycamargo/dotfiles/refs/heads/master/terminal-setup.ps1"
-Invoke-Expression ((new-object net.webclient).DownloadString($url))
-
