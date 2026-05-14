e1af98b99482373ae0d7117ca312c8c58771b0ab
Wesley Camargo <camargo.wes@gmail.com>
2026-05-14 19:40:30 +0200

    initial commit

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
-set -e
+#!/usr/bin/env sh
+set -eu
 
-DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
 echo "Installing personal dotfiles from $DOTFILES_DIR"
 
