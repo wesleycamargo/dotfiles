ae93c8e0be725fc1f88e46dcafff6c1f5e3b7461
Wesley Camargo <camargo.wes@gmail.com>
2026-05-14 20:47:25 +0200

    install.ps1

---
+++ install.ps1
@@ -0,0 +1,1 @@
+oh-my-posh --init --shell pwsh --config https://gist.githubusercontent.com/wesleycamargo/06b58b472fe0cded2e6d6451ea0778bd/raw/1c952297121a50826ef6f849bb7ee8c11d8e09a6/oh-my-posh-az-cli-az-pwsh.json | Invoke-Expression
